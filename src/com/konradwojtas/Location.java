b59e11473c3ba084bd2d031a2cade329f4528b6e
Konrad Wojtas <kwojtas.pl@gmail.com>
2018-08-19 16:56:22 +0200

    first commit

---
+++ src/com/konradwojtas/Location.java
@@ -0,0 +1,33 @@
+package com.konradwojtas;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Location {
+    private final int locationID;
+    private final String description;
+    private final Map<String, Integer> exits;
+
+    public Location(int locationID, String description) {
+        this.exits = new HashMap<String, Integer>();
+        this.locationID = locationID;
+        this.description = description;
+    }
+
+
+    public void addLocation(String direction, int location) {
+        exits.put(direction, location);
+    }
+
+    public int getLocationID() {
+        return locationID;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public Map<String, Integer> getExits() {
+        return new HashMap<>(this.exits);
+    }
+}
